736145f4c23ed5a957633a351fd2b1bd075a9aeb
Josh Palay <joshpalay@gmail.com>
2014-01-31 17:23:19 -0500

    Added intro animation

--- animation.js
+++ animation.js
@@ -1,12 +1,23 @@
-
 // Opening Fade In: THE HARVARD CRIMSON :)
 $(function() {
+    setTimeout(function() {
+        $('.overlay').hide();
+    }, 0);
     $('.opening').fadeIn(2500);
-    setTimeout(function(){$('.cute').fadeIn('slow')},2000);
-    setTimeout(function(){$('#link').fadeIn('fast')},3000);
+    setTimeout(function() {
+        $('.cute').fadeIn('slow');
+    }, 2000);
+    setTimeout(function() {
+        $('#msg').fadeIn('fast');
+    }, 3000);
+    setTimeout(function() {
+        $('#splash').fadeOut();
+        $('.overlay').show();
+    }, 6000);
+    
+    $(window).keydown(function() {
+        $('.overlay').show();
+        $('#splash').fadeOut('slow');
+    });
+    
 });
-
-
-
-
-
