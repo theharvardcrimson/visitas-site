3d52667bfc506379cd6bb4478be7b8af13de405c
Sam Fishman <sfishman@college.harvard.edu>
2014-01-31 20:56:27 -0500

    updated templating and styling

--- template.py
+++ template.py
@@ -9,6 +9,9 @@ env = Environment()
 env.loader = FileSystemLoader('.')
 cwd = os.getcwd()
 
+if not os.path.exists('rendered/'):
+	os.mkdir('rendered/')
+
 for root, dirs, files in os.walk(cwd):
 	if 'rendered' in dirs:
 		dirs.remove('rendered')
@@ -21,16 +24,22 @@ for root, dirs, files in os.walk(cwd):
 		else:
 			fname = f
 
-		if not fname == "base.html.tmp":
+		if not (f == "base.html.tmp" or f == __file__[2:]):
 			if fname.find('.html.tmp') != -1:
-				path_start = 'rendered/' + fname[:-9]
-				out_name = path_start + '/index.html'
-				if not os.path.exists(path_start):
-					os.makedirs(path_start)
-				contents = env.get_template('/' + fname).render(__path__=out_name)
+				if fname != "index.html.tmp":
+					path_start = 'rendered/' + fname[:-9]
+					out_name = path_start + '/index.html'
+					if not os.path.exists(path_start):
+						os.makedirs(path_start)
+				else:
+					out_name = 'rendered/' + fname[:-4]
+				url_path = out_name[out_name.find('/'):out_name.rfind('/') + 1]
+				contents = env.get_template('/' + fname).render(__path__=url_path)
 			else:
 				out_name = 'rendered/' + fname
 				with open(fname) as orig:
 					contents = orig.read()
+				if not os.path.exists(out_name[:out_name.rfind('/')]):
+					os.makedirs(out_name[:out_name.rfind('/')])
 			with open(out_name, 'w+') as out_file:
 				out_file.write(contents)
