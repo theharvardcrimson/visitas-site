3860a115a348d57b8f42943f8f99c7c8fd9e6b27
Nikhil Benesch <nikhil.benesch@gmail.com>
2015-09-06 17:19:23 -0400

    fix shebang

--- template.py
+++ template.py
@@ -1,4 +1,4 @@
-#!/Users/sfishman/.venvs/general/bin/python
+#!/usr/bin/env python
 from jinja2 import FileSystemLoader, Template
 from jinja2.environment import Environment
 from datetime import date
