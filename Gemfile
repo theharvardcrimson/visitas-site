42385863761158c35aed989d5b168867628098cd
Nenya Edjah <nenyaedjah@gmail.com>
2018-01-22 11:59:21 -0500

    removed xhtml. added Gemfile. added readme

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+
+gem 's3_website', '~> 3.4.0'
--- template.py
+++ template.py
@@ -32,7 +32,7 @@ for root, dirs, files in os.walk(cwd):
 			fname = root.replace(cwd, '')[1:] + '/' + f
 		else:
 			fname = f
-			
+
 		if not (f == "base.html.tmp" or f == __file__[2:]):
 			if fname.find('.html.tmp') != -1:
 				if fname != "index.html.tmp":
@@ -43,12 +43,14 @@ for root, dirs, files in os.walk(cwd):
 				else:
 					out_name = 'rendered/' + fname[:-4]
 				url_path = out_name[out_name.find('/'):out_name.rfind('/') + 1]
-				contents = env.get_template('/' + fname).render(__path__=url_path, subtitle=subtitle)
+				template = env.get_template('/' + fname)
+				contents = template.render(__path__=url_path, subtitle=subtitle)
+				contents = contents.encode()
 			else:
 				out_name = 'rendered/' + fname
-				with open(fname) as orig:
+				with open(fname, 'rb') as orig:
 					contents = orig.read()
 				if not os.path.exists(out_name[:out_name.rfind('/')]):
 					os.makedirs(out_name[:out_name.rfind('/')])
-			with open(out_name, 'w+') as out_file:
+			with open(out_name, 'wb+') as out_file:
 				out_file.write(contents)
